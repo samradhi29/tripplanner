fa1f80588845f0fdbf25a48c32f74151d4a06fe8
samradhi29 <samradhi4320@gmail.com>
2025-06-14 13:39:15 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import Herosection from './components/Herosection'
 import Destinationpage from './components/Destinationpage'
 import Advantages from './components/Advantages'
-import Packagespage from './components/packagespage'
+import Packagespage from './components/Packagespage'
 import Testimonials from './components/Testimonial'
 export default function App() {
   return (
